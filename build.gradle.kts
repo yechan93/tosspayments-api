f231a137c15f78def51fbbcf520c3f4a3ec17bb9
yechan93 <dhkfhdhkf@gmail.com>
2026-06-05 21:53:23 +0900

    feat: 프로젝트 초기세팅

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.14"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.tosspayments"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-batch")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.batch:spring-batch-test")
+	testCompileOnly("org.projectlombok:lombok")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testAnnotationProcessor("org.projectlombok:lombok")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
